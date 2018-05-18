0aad0f7095ffc9a8d188b96849c8ba34e2494614
Funchs <renqiang@58ganji.com>
2018-05-18 14:45:17 +0800

    init

---
+++ src/main/java/org/springframework/social/qq/SpringSocialQqApplication.java
@@ -0,0 +1,12 @@
+package org.springframework.social.qq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSocialQqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSocialQqApplication.class, args);
+	}
+}
